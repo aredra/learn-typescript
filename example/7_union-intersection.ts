f137e197396b515a6ef48adfd318d416eddaa87f
Jiyoung Seo <rnswn48@gmail.com>
2021-01-16 21:54:21 +0900

    Add class examples

--- example/7_union-intersection.ts
+++ example/7_union-intersection.ts
@@ -1,17 +1,17 @@
-function logMessage(value: string) {
-  console.log(value);
-}
-function logMessage(value: number) {
-  console.log(value);
-}
-function logMessage(value: any) {
-  console.log(value);
-}
+// function logMessage(value: string) {
+//   console.log(value);
+// }
+// function logMessage(value: number) {
+//   console.log(value);
+// }
+// function logMessage(value: any) {
+//   console.log(value);
+// }
 
 // # Union 타입 문법 - `any` 보다는 명시적임
-function logMessage(value: string | number) {
-  console.log(value);
-}
+// function logMessage(value: string | number) {
+//   console.log(value);
+// }
 
 function logMessage(value: string | number) {
   if (typeof value === 'string') {
@@ -36,5 +36,47 @@ interface Person {
 
 function askSomeone(someone: Developer | Person) {
   someone.name; // O
-  someone.age; // X
+//  someone.age; // X
 }
+
+function askSomeone2(someone: Developer & Person) {
+  someone.name;
+  someone.age;
+  someone.skill;
+}
+
+enum Shoes {
+  Nike,
+  Adidas
+}
+
+let myShoes = Shoes.Nike;
+console.log(myShoes);
+
+enum Answer {
+  Yes = 'Y',
+  No = 'N'
+}
+
+function askQeustion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log('correct');
+  }
+  if (answer == Answer.No) {
+    console.log('wrong');
+  }
+}
+
+askQeustion(Answer.Yes);
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    console.log('created person');
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const aredra = new Person('aredra', 99);
